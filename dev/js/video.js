096a22c8a093b94c00970e9378328eb290b74533
Espen Bang <espenbang@Espens-MacBook-Pro.local>
2017-02-21 10:41:34 +0100

    Videoplayer for mobile view

--- dev/js/video.js
+++ dev/js/video.js
@@ -1,13 +1,29 @@
 window.onload = function() {
 
-  var video = document.getElementById("video");
+  var video = document.getElementById("videoPlayer");
+  var mobileVideo = document.getElementById("mobileVideoPlayer");
+  var background = document.getElementById("videoBackground");
+
   var muteButton = document.getElementById("muteButton");
+  var playButton = document.getElementById("playButton");
 
   muteButton.addEventListener("click", function() {
     if (video.muted === false) {
         video.muted = true;
     } else {
       video.muted = false;
+    }
+  });
+
+  playButton.addEventListener("click", function() {
+    if (mobileVideo.paused === true) {
+
+      background.style.display = "none";
+      playButton.style.visibility = "hidden";
+
+      mobileVideo.play();
+    } else {
+      mobileVideo.pause();
     }
   });
 }
